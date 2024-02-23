a7629c957d7c8c3524c16fae9409e52774e212df
KRISHANSHARMA02 <114760812+KRISHANSHARMA02@users.noreply.github.com>
2024-02-23 16:17:56 +0530

    Update files: globals.css, Card.js, Foreground.js, package-lock.json, package.json

--- components/Card.js
+++ components/Card.js
@@ -1,19 +1,32 @@
 "use client"
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
-const Card = ({data}) => {
+const Card = ({data,reference}) => {
   return (
-    <div className='relative w-60 h-72 rounded-[30px] bg-zinc-900/90 text-white p-6 overflow-hidden'>
+    <motion.div drag dragConstraints={reference} whileDrag={{scale:1.1}} dragElastic={0.1}   dragTransition={{ bounceStiffness: 100, bounceDamping: 30 }} className='relative w-60 h-72 rounded-[30px] bg-zinc-900/90 text-white p-6 overflow-hidden'>
       <FaRegFileAlt />
       <p className='text-xs leading-tight mt-5 font-semibold' >{data.desc} </p>
       <div className='footer w-full absolute bottom-0 left-0 '>
-        <div className=''></div>
+        <div className='flex items-center justify-between w-full px-8 py-3 mb-3' >
+          <h5>{data.filesize} </h5>
+          <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
+            {data.close?<IoClose />:<LuDownload size=".7rem" color='#fff'/>}
+          </span>
+          </div>
+          {data.tag.isOpen && (
+         <div className={`tag w-full ${data.tag.tagColor==="blue"?"bg-blue-600": "bg-green-600"} flex justify-center items-center py-4`}>
+          <h5 className=' text-sm font-semibold'>{data.tag.tagTitle}</h5>
+          </div> 
+
+          )}
       </div>
-           data 
-           seekbar
-    </div>
-  )
+           
+    </motion.div>
+  );
 }
 
 export default Card
--- components/Foreground.js
+++ components/Foreground.js
@@ -1,21 +1,40 @@
 "use client"
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 
 const Foreground = () => {
+  const ref = useRef(null);
     const data = [
+      {
+        desc :"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        filesize :"0.1mb",
+        close : false,
+        tag : {isOpen:true, tagTitle: "Download Now", tagColor:"green" },
+      },
       {
         desc :"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
         filesize :"0.9mb",
         close : true,
+        tag : {isOpen:false, tagTitle: "Update", tagColor:"blue" },
+      },
+      {
+        desc :"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        filesize :"0.10mb",
+        close : true,
         tag : {isOpen:true, tagTitle: "Download Now", tagColor:"green" },
+      },
+      {
+        desc :"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        filesize :"0.5mb",
+        close : true,
+        tag : {isOpen:true, tagTitle: "Download Now", tagColor:"blue" },
       },
     ];
 
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-screen '>
+    <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-screen p-5 flex gap-4 flex-wrap '>
       {data.map((item,index)=>(
-        <Card data={item} />
+        <Card data={item} reference={ref}/>
 
       ))}
     </div>
